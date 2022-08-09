a29488e8cd2664f574f4165ef25ba1240a4e3be5
omarjab01 <omar241101skate@gmail.com>
2022-08-09 09:51:31 +0200

    pubblicità e tracking

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,13 +1,51 @@
 import '../styles/global.scss'
 import React, {useEffect, useState} from 'react';
 import { Layout } from '../components';
+import { useRouter } from 'next/router'
+import * as gtag from '../lib/gtag'
 
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter()
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url)
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    router.events.on('hashChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+      router.events.off('hashChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
   return (
+    <>
+      {/* Global Site Tag (gtag.js) - Google Analytics */}
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${gtag.GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
+      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3722236717656068"
+     crossorigin="anonymous"></script>
     <Layout>
       <Component {...pageProps} />
     </Layout>
+    </>
   )
 }
 
